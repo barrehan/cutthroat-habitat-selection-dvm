6289afa51708d8baf8f7200f047763b410af0f5d
barrehan <95871302+barrehan@users.noreply.github.com>
2022-09-27 16:08:16 -0700

    code

---
+++ code/analysis/avg.cv.loggers.vs.fish.r
@@ -0,0 +1,8 @@
+library(readr)
+library(tidyverse)
+library(lubridate)
+library(dplyr)
+library(ggplot2)
+
+setwd("C:/Users/barrehan/GitHub/projects/cwa.habitat.selection")
+mouth<- read.csv("data/temp.do.data/do.temp.combined/blue.ruin.temp.mainstem.csv")
