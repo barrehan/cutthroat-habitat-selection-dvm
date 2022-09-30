3569e7dec381096bab9585d2b3937f2fd81d2ad2
barrehan <95871302+barrehan@users.noreply.github.com>
2022-09-30 14:11:55 -0700

    code.regression

--- code/cleanup/logger.regression.br.array.2.mid.low.do.temp.r
+++ code/cleanup/logger.regression.br.array.2.mid.low.do.temp.r
@@ -49,6 +49,9 @@ ggplot()+
   geom_point(data=mid.log, aes(x = date.time, y = dissolved.oxygen), colour = "blue")+
   geom_point(data=mid.rep, aes(x = date.time, y = dissolved.oxygen), colour = "pink")
 
+#'temp is definitely higher, DO is less variable (less sun/less photosynthesis?)
+#'checking out the site 3 array bottom and mid logger temps/dos
+
 site.3.array<-read.csv("data/raw.data/logger.array/blue.ruin.site.3.mid.temp.do.csv")
 site.3.array$date.time <-mdy_hm(site.3.array$date.time)
 site.3.array <- site.3.array %>% force_tz(site.3.array$date.time, tzone = "America/Los_Angeles")
@@ -61,10 +64,14 @@ ggplot(mid.low.3, aes(x = date.time, y = temperature, colour = logger.position))
 site.3.mid<-site.3.array[site.3.array$logger.position == "mid.do",]
 site.3.bottom <- site.3.array[site.3.array$logger.position == "bottom.do",]
 
+#'looking at original array mid do logger (blue) and comparing to site 3 mid and  
+#'bottom logger
 ggplot()+
   geom_point(data = mid.log, aes(x = date.time, y = temperature), colour = "blue")+
   geom_point(data = site.3.mid, aes(x = date.time, y= temperature), colour = "red")+
   geom_point(data = site.3.bottom, aes(x = date.time, y = temperature), colour= "dark green")
+
+#'mid loggers from site 3 and site 2 are kind of similar, temperature definitely higher at site 3
 
 ggplot()+
   geom_point(data = mid.log, aes(x = date.time, y = dissolved.oxygen), colour = "blue")+
@@ -81,3 +88,77 @@ ggplot(data = original.array, aes(x = date.time, y =dissolved.oxygen, group = se
   geom_point(aes(colour = sensor.depth))
 
 #'linear relationship between mid and lower logger do?
+#'reshape long to wide format
+
+rep.temp <- array %>%select(date.time,logger.position, temperature)
+rep.do <- array %>% select(date.time, logger.position, dissolved.oxygen)
+
+temp <- spread(rep.temp, key = logger.position, value = temperature)
+do <- spread(rep.do, key = logger.position, value = dissolved.oxygen)
+temp <- temp %>%
+  rename(
+    bottom.temp = bottom.do,
+    mid.temp = mid.do)
+
+do.lm <- lm(bottom.do~mid.do, data = do)
+summary(do.lm)
+
+ggplot(data = do, aes(x = mid.do, y = bottom.do))+
+  geom_point()+
+  geom_smooth(method = 'lm')
+
+temp.lm <-lm(bottom.temp~mid.temp, data = temp)
+summary(temp.lm)
+
+ggplot(data = temp, aes(x = mid.temp, y = bottom.temp))+
+  geom_point()+
+  geom_smooth(method = 'lm')
+
+#' looks like there is a solid linear relationship between mid and bottom
+#' do/temp logger at site 2, will use the intercept/estimate to back calculate
+#' do/temp for bottom logger at site 2 during the study using the mid logger data
+#' during that time frame
+
+mid.log <- mid.log %>%select(date.time,dissolved.oxygen, temperature)
+mid.log <- mid.log %>%
+  rename(
+    mid.do = dissolved.oxygen,
+    mid.temp = temperature)
+
+m.do <- 0.9633
+b.do <- -0.7434
+mid.log$bottom.do.est <- m.do*mid.log$mid.do + b.do
+
+m.temp <- 0.813
+b.temp <- 2.169
+mid.log$bottom.temp.est <- m.temp*mid.log$mid.temp + b.temp
+
+mid.log <- mid.log %>% select(date.time, bottom.do.est, bottom.temp.est)
+
+bottom.log <- mid.log %>%
+  rename(
+    dissolved.oxygen = bottom.do.est,
+    temperature = bottom.temp.est)
+
+bottom.log$logger.site <- "site.2"
+bottom.log$logger.position <- "bottom.do"
+bottom.log$sensor.depth <- as.factor(3.6)
+bottom.log$site.total.depth <- 3.8
+bottom.log$logger.type <- "NA"
+bottom.log$latitude<- 44.23156
+bottom.log$longitude<- -123.1628
+bottom.log$logger.id <-"NA"
+
+new.site.2<- rbind(original.array, bottom.log)
+
+#' ok bottom temp sensor is recording lower temperatures than the bottom DO 
+#' logger (which is deeper)...going to remove recordings from this temp only
+#' sensor
+
+new.site.2 <- new.site.2[!new.site.2$sensor.depth == 2.85,]
+
+new.site.2 <- new.site.2[
+  with(new.site.2, order(date.time, sensor.depth)),
+]
+
+write.csv(new.site.2, "data/modif.data/logger.array/blue.ruin.site.2.array.do.temp.csv", row.names = F)
