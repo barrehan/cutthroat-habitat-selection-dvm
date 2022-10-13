ac8d39e82d622cbff4964ef03cc3b1c97e9e648d
barrehan <95871302+barrehan@users.noreply.github.com>
2022-10-13 14:19:18 -0700

    code.plot.depth.rt

--- code/analysis/radio.tag.fish/assign.fish.array.interpolate.depth.do.r
+++ code/analysis/radio.tag.fish/assign.fish.array.interpolate.depth.do.r
@@ -17,7 +17,8 @@ library(lubridate)
 library(dplyr)
 library(data.table)
 library(rgdal)
-
+library(cowplot) #grid ggplots
+library(viridis)
 #'clear workspace
 rm(list = ls())
 #close open graphics devices
@@ -27,7 +28,10 @@ graphics.off()
 setwd("C:/Users/barrehan/GitHub/projects/cwa.habitat.selection")
 
 # Create Blue Ruin polygon with points for arrays/receivers ---------------
-
+#'import all fish tag data corrected to all occur at ten minute intervals
+fish.dat <- read.csv('data/modif.data/radio.tag/tag.reads.10.min.interval.csv')
+fish.dat$date.time = mdy_hm(fish.dat$date.time)
+fish.dat <- fish.dat %>% force_tz(fish.dat$date.time, tzone = "America/Los_Angeles")
 #'import array and receiver location data
 receiver.dat <- unique(fish.dat[c("receiver.site", "antenna.number", "lat", 'long')])
 receiver.dat<- receiver.dat %>%arrange(receiver.site, antenna.number)
@@ -83,10 +87,7 @@ map.points <- ggplot() +
 #' take fish radio tag data and array data to interpolate fish depth/do using 
 #' fish temp and nearest array
 
-#'import all fish tag data corrected to all occur at ten minute intervals
-fish.dat <- read.csv('data/modif.data/radio.tag/tag.reads.10.min.interval.csv')
-fish.dat$date.time = mdy_hm(fish.dat$date.time)
-fish.dat <- fish.dat %>% force_tz(fish.dat$date.time, tzone = "America/Los_Angeles")
+
 
 #'assign closest logger array id
 fish.dat$logger.site<- ifelse(fish.dat$receiver.site == 1 & fish.dat$antenna.number ==1, "site.0.mouth",
@@ -119,6 +120,8 @@ arrays <- arrays %>% force_tz(arrays$date.time, tzone = "America/Los_Angeles")
 #'curtail fish dat to narrowest array timeframe 7/26 - 8/15
 fish <- fish.dat[fish.dat$date.time >= "2021-07-26 00:00:00" & fish.dat$date.time < "2021-08-15 07:50:00",]
 
+#remove NA temp (from site 4/5 do/temp combination)
+arrays<-arrays[!is.na(arrays$temperature),]
 
 # For loop to interpolate fish depth/do using closest array ---------------
 
@@ -126,15 +129,53 @@ fish <- fish.dat[fish.dat$date.time >= "2021-07-26 00:00:00" & fish.dat$date.tim
 #if s1-s4 use interpolation to determine depth
 for(i in 1:nrow(fish)){
   row <- fish[i,]
-  t1 <-row$temp.strong                                                 #fish temperature
-  t.match <- arrays[arrays$date.time == row$date.time,]                #find matching time stamp
-  a.match <- t.match[t.match$logger.site == row$logger.site,]          #find matching array
-  x1 <- max(a.match$temperature[which(a.match$temperature < t1)])      #find the next closest temperature less than ibutton temp
-  x2 <- min(a.match$temperature[which(a.match$temperature > t1)])      #find the next closest temperature greater than ibutton temp
-  d1 <- a.match$sensor.depth[a.match$temperature == x1]
-  d2 <- a.match$sensor.depth[a.match$temperature == x2]
-  d <- (d2-d1)/(x2-x1)*(t1 - x1) + d1                                  #slope equation to calculate fish depth based on sensor depth/temp
-  m <- 'mouth.mixed'
-  do <-ifelse(row$receiver.site == 1 & row$antenna.number ==1, m, d)   #if closest array is temp sensor at mouth, depth is NA
-  fish[i,11] <-do             
+  t1 <-row$temp.strong                                                    #fish temperature
+  t.match <- arrays[arrays$date.time == row$date.time,]                   #find matching time stamp
+  a.match <- t.match[t.match$logger.site == row$logger.site,]             #find matching array
+  x1 <- max(a.match$temperature[which(a.match$temperature < t1)])         #find the closest temperature less than ibutton temp (deeper sensor)
+  x2 <- min(a.match$temperature[which(a.match$temperature > t1)])         #find the closest temperature greater than ibutton temp (shallower sensor)
+  d1 <- a.match$sensor.depth[a.match$temperature == x1]                   #depth of deeper sensor (associated with x1)
+  d2 <- a.match$sensor.depth[a.match$temperature == x2]                   #depth of shallower sensor (associated with x2)
+  d <- (d2-d1)/(x2-x1)*(t1 - x1) + d1                                     #slope equation to calculate fish depth based on sensor depth/temp
+                                                                          #what if the fish temperature is warmer than shallowest sensor/colder than deepest sensor?
+  deepest <- a.match[which.max(a.match$sensor.depth),]                    #reference line with deepest sensor
+  dep<-deepest$sensor.depth                                               #pull sensor depth information
+  shallowest <-a.match[which.min(a.match$sensor.depth),]                  #reference line with shallowest sensor
+  shal<-shallowest$sensor.depth                                           #pull sensor depth information
+  d<-ifelse(is_empty(d1), dep, d)                                         #if fish temp is warmer than shallowest logger temp, assign that logger depth
+  d<-ifelse(is_empty(d2), shal, d)                                        #if fish temp is colder than deepest logger temp, assign that logger depth
+  m <- 'mouth.mixed'                                                   
+  depth <-ifelse(row$receiver.site == 1 & row$antenna.number ==1, m, d)   #if closest array is temp sensor at mouth, there is no fish depth (mixed water column...)
+  
+  fish[i,11] <-depth            
 }
+
+colnames(fish)[11] <- "interp.fish.depth"
+
+#'order df by tag id and then by date
+
+fish <- fish[
+  order(fish[,2], fish[,1] ),
+]
+
+fishes<- fish
+fishes <- mutate_at(fishes, vars(receiver.site), as.factor)
+t11<- fishes[fishes$tag.id == 11,]
+t11<-t11[!t11$interp.fish.depth == "mouth.mixed",]
+t11$interp.fish.depth<- as.numeric(t11$interp.fish.depth)
+t11$receiver.site <-fct_rev(t11$receiver.site)
+
+s11<-ggplot(t11, aes (date.time, receiver.site, color = temp.strong))+
+  geom_point()+
+  geom_line()+
+  scale_y_reverse()+
+  scale_color_viridis(option = "inferno")
+  
+d11<-ggplot(t11, aes (date.time, interp.fish.depth, color = temp.strong))+
+  geom_point()+
+  geom_line()+
+  scale_y_reverse()+
+  scale_color_viridis(option = "inferno")
+
+plot_grid(s11,d11,labels = c("Receiver site", "Fish depth"), ncol = 2, nrow =1)
+
