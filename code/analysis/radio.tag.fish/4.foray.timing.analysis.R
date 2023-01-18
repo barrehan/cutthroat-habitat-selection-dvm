765ddbb2bcab13886d67f91ba273958c3416fd2a
barrehan <95871302+barrehan@users.noreply.github.com>
2023-01-18 12:15:41 -0800

    foray.hist

--- code/analysis/radio.tag.fish/4.foray.timing.analysis.R
+++ code/analysis/radio.tag.fish/4.foray.timing.analysis.R
@@ -2,11 +2,13 @@
 #' Analyze foray files to see if there is any pattern to when fish exit/return
 #' to BR alcove
 
+library(plyr)
 library(dplyr)
 library(tidyverse)
 library(lubridate)
 library(ggplot2)
-library(plyr)
+library(pals) #color palettes discrete colors
+
 
 #'clear workspace
 rm(list = ls())
@@ -27,21 +29,28 @@ min.120 <- results[,c(1, 2, 21, 22, 32)]
 min.120 <- min.120[min.120$foray.120 ==1,]
 length(unique(min.120$tag.id))
 
-exit <- read.csv("data/modif.data/radio.tag/mainstem.foray/foray.deets/all.tags.last.read.csv")
-exit$date.time <- mdy_hm(exit$date.time)
-exit <- exit %>% force_tz(exit$date.time, tzone = "America/Los_Angeles")
-
-exit <- exit[,c(1,2,8)]
-
 min.120<-min.120[,c(1,2)]
-min.120$exit <- 'n'
-foray <-rbind(min.120, exit)
+min.120$exit <- NA
+min.120$tag.id <- as.factor(min.120$tag.id)
+min.120$date <- as.Date(min.120$date.time)
 
-foray$tag.id <-as.factor(foray$tag.id)
-foray$exit <-as.factor(foray$exit)
-
-ggplot(data = min.120, aes(x = date.time, fill = tag.id, color = exit)) +
+ggplot(data = min.120, aes(date, fill = tag.id)) +
   geom_histogram(position = "stack")+
-  scale_color_brewer(palette="Dark2")
+  scale_fill_manual(values = as.vector(glasbey(28)))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
 
-#'black outline around instance of final exit 
+ex <- read.csv("data/modif.data/radio.tag/mainstem.foray/all.tags.last.read.csv")
+ex$date.time <- mdy_hm(ex$date.time)
+ex <- ex %>% force_tz(ex$date.time, tzone = "America/Los_Angeles")
+ex<- ex[,c(1,2,8)]
+ex$tag.id <- as.factor(ex$tag.id)
+ex$date <- as.Date(ex$date.time)
+  
+ggplot(data = ex, aes(date, fill = tag.id, color = exit))+
+  geom_bar(size = 1)+
+  scale_fill_manual(values = as.vector(glasbey(28)))+
+  scale_color_manual(values = c("yellow", "black", "red"))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+
