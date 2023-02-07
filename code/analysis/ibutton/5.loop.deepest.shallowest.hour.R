4c53927c1f62e183d7e42499d3c2eda63983f5e9
Hannah Barrett <95871302+barrehan@users.noreply.github.com>
2023-02-07 14:24:33 -0800

    deepest.shallowest.hist

--- code/analysis/ibutton/5.loop.deepest.shallowest.hour.R
+++ code/analysis/ibutton/5.loop.deepest.shallowest.hour.R
@@ -9,15 +9,21 @@ library(viridis)
 library(dplyr)
 library(tidyverse)
 library(lubridate)
+library(chron)
 
 setwd("C:/Users/barrehan/GitHub/projects/cwa.habitat.selection")
 
 fish <-read.csv("data/modif.data/hab.select.mod/br.ibutton.data/br.ibutton.pooled.csv")
-
+fish$date.time<-ymd_hms(fish$date.time)
 fish <-fish[fish$case ==1,]
 fish<-fish[,c(1:4,6)]
 fish$hour <-hour(fish$date.time)
 fish$date <-as.Date(fish$date.time)
+fish$time <-fish$date.time
+fish$time <- format(as.POSIXct(fish$date.time), format = "%H:%M:%S")
+fish$fake.date <- "2021-01-01"
+fish$time2 <- paste(fish$fake.date, fish$time)
+fish$time2 <-ymd_hms(fish$time2)
 
 buttons <-unique(fish$ibutton.id)
 dates <- unique(fish$date)
@@ -46,4 +52,41 @@ for(i in 1:length(buttons)){
 }
 
 
+shallow <-dat[dat$min.max == "shallowest",]
+deep <- dat[dat$min.max == "deepest",]
+
+ggplot(data = shallow, aes(x =hour))+
+  geom_histogram(binwidth = 1, boundary = -7.5, colour = "black", fill = "lightsteelblue",size = .2)+
+  coord_polar()+
+  scale_x_continuous(limits = c(0, 24),
+                     breaks = seq(0,24, by = 4),
+                     minor_breaks = seq(0,24, by = 1))+
+  theme_bw()+
+  theme(panel.border = element_blank())+
+  ggtitle("Daily timing most shallow water column position")
+
+ggplot(data = deep, aes(x =hour))+
+  geom_histogram(binwidth = 1, boundary = -7.5, colour = "black", fill = "red4",size = .2)+
+  coord_polar()+
+  scale_x_continuous(limits = c(0, 24),
+                     breaks = seq(0,24, by = 4),
+                     minor_breaks = seq(0,24, by = 1))+
+  theme_bw()+
+  theme(panel.border = element_blank())+
+  ggtitle("Daily timing deepest water column position")
+
+ggplot(data = fish, aes (x = time2, y = depth))+
+  geom_point()+
+  geom_smooth(colour = "seagreen4", fill = "burlywood")+
+  scale_y_reverse()+
+  scale_x_datetime(
+    breaks = "2 hours",
+    date_labels = "%H")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+  ggtitle("Smoothed fit fish depth across 24-hour period, Blue Ruin netpens")+
+  xlab("Time")+
+  ylab("Depth (m)")
+
+  
 
